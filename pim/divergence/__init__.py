e8198bd014e0ab037c82a68dbb28aff02e34c855
PierreColombo <colombo.pierre@gmail.com>
2022-08-03 21:22:00 -0400

    fix paths

--- pim/divergence/__init__.py
+++ pim/divergence/__init__.py
@@ -1,1 +1,7 @@
 from ab_div import ABDivergence
+from alpha_div import AlphaDivergence
+from beta_div import BetaDivergence
+from fisher_rao import FisherRao
+from kl_div import KullbackLeiblerDivergence
+from lp import LP
+from renyi_div import RenyiDivergence
